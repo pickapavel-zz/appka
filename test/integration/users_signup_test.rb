8974d721e3a91acbd38e171d6084cf1f6ae53770
pici <pickapavel@gmail.com>
2016-11-11 15:07:24 +0000

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
+ 
     assert_template 'users/new'
-    assert_not flash.FILL_IN
+    
 
     
   end
